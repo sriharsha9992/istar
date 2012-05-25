47f20ece7cfc50d6f6a23fc2faf2a09906789ce9
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-05-26 07:14:50 +0800

    Abstracted job creation

--- contextio.js
+++ contextio.js
@@ -3,7 +3,9 @@
 
 var fs = require('fs'),
     path = require('path'),
-    ContextIO = require('contextio');
+    ContextIO = require('contextio'),
+//	validator = require('node-validator'),
+	job = require('./job.js');
 
 // Initialize the context.io client
 var ctxioClient = new ContextIO.Client({
@@ -16,25 +18,26 @@ var offset = 0;
 
 // Get messages every minute
 //setInterval(function() {
-  ctxioClient.accounts(process.argv[4]).messages().get({ offset: offset }, function (err, res) {
+  ctxioClient.accounts(process.argv[4]).messages().get({ include_body: 1, body_type: 'text/plain', offset: offset, limit: 10 }, function (err, res) {
     if (err) throw err;
     res.body.forEach(function(msg) {
-	  console.log(msg.subject);
-	  console.log(msg.addresses.from.email);
-	  msg.files.forEach(function(file) {
-	    if (file.size > 10485760) return;
-        ctxioClient.accounts(process.argv[4]).files(file.file_id).content().get(function (err, res) {
-          if (err) throw err;
-	      switch (path.extname(file.file_name)) {
-	        case '.pdbqt':
-	          fs.writeFile(file.file_name, res.body);
-              break;
-			case '.txt':
-              var config = JSON.parse(res.body.toString());
-              console.log(config.center_x);
-              break;
-          }
-        });
+      var config = JSON.parse(msg.body[0].content);
+      // Validate user input
+      if (msg.files.length !== 1) return;
+	  var file = msg.files[0];
+      if (file.size > 10485760) return;
+      ctxioClient.accounts(process.argv[4]).files(file.file_id).content().get(function (err, res) {
+        if (err) throw err;
+        job.create({
+          center_x: config.center_x,
+          center_y: config.center_y,
+          center_z: config.center_z,
+          size_x: config.size_x,
+          size_y: config.size_y,
+          size_z: config.size_z,
+          description: msg.subject,
+	      email: msg.addresses.from.email
+	    }, res.body);
       });
     });
     offset += res.body.length; // Number of messages actually returned
