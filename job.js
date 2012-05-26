8382b21b0a4ddef76b89d1e3b091be362775c0e4
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-05-26 08:37:33 +0800

    Fixed a tiny bug.

--- job.js
+++ job.js
@@ -5,11 +5,11 @@ exports.insert = function (job, receptor) {
   new mongodb.Db('istar', new mongodb.Server('137.189.90.124', 27017)).open(function(err, db) {
     db.authenticate('daemon', '2qR8dVM9d', function(err, result) {
       db.collection('jobs', function(err, collection) {
-        // Add additional fields for job
+        // Add additional fields for job, like time = Date.now()
         collection.insert(job, { safe: true }, function(err, docs) {
           db.close();
           var _id = docs[0]._id,
-              folder = __dirname + '/public/jobs/' + _id,
+              folder = __dirname + '/public/jobs/' + _id;
           fs.mkdir(folder, function() {
             fs.writeFile(folder + '/receptor.pdbqt', receptor);
           });
