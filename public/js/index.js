2b7cf3df59666b844caa5583e7a5cede09312109
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-07-02 10:54:42 +0800

    Reordered the 9 molecular properties

--- public/js/index.js
+++ public/js/index.js
@@ -27,11 +27,17 @@ $(function() {
     max: 12,
     values: [ 0, 5 ]
   });
-  $('#nrb').slider({
+  $('#ad').slider({
     range: true,
-    min: 0,
-    max: 34,
-    values: [ 2, 8 ]
+    min: -25,
+    max: 29,
+    values: [ 0, 12 ]
+  });
+  $('#pd').slider({
+    range: true,
+    min: -504,
+    max: 1,
+    values: [ -50, 0 ]
   });
   $('#hbd').slider({
     range: true,
@@ -45,29 +51,23 @@ $(function() {
     max: 18,
     values: [ 2, 10 ]
   });
+  $('#tpsa').slider({
+    range: true,
+    min: 0,
+    max: 317,
+    values: [ 20, 100 ]
+  });
   $('#charge').slider({
     range: true,
     min: -5,
     max: 5,
     values: [ 0, 0 ]
   });
-  $('#ad').slider({
-    range: true,
-    min: -25,
-    max: 29,
-    values: [ 0, 12 ]
-  });
-  $('#pd').slider({
-    range: true,
-    min: -504,
-    max: 1,
-    values: [ -50, 0 ]
-  });
-  $('#tpsa').slider({
+  $('#nrb').slider({
     range: true,
     min: 0,
-    max: 317,
-    values: [ 20, 100 ]
+    max: 34,
+    values: [ 2, 8 ]
   });
   $('.slider').slider({
     slide: function(event, ui) {
@@ -80,20 +80,20 @@ $(function() {
         mwt_ub: $('#mwt_ub').text(),
         logp_lb: $('#logp_lb').text(),
         logp_ub: $('#logp_ub').text(),
-        nrb_lb: $('#nrb_lb').text(),
-        nrb_ub: $('#nrb_ub').text(),
-        hbd_lb: $('#hbd_lb').text(),
-        hbd_ub: $('#hbd_ub').text(),
-        hba_lb: $('#hba_lb').text(),
-        hba_ub: $('#hba_ub').text(),
-        charge_lb: $('#charge_lb').text(),
-        charge_ub: $('#charge_ub').text(),
         ad_lb: $('#ad_lb').text(),
         ad_ub: $('#ad_ub').text(),
         pd_lb: $('#pd_lb').text(),
         pd_ub: $('#pd_ub').text(),
+        hbd_lb: $('#hbd_lb').text(),
+        hbd_ub: $('#hbd_ub').text(),
+        hba_lb: $('#hba_lb').text(),
+        hba_ub: $('#hba_ub').text(),
         tpsa_lb: $('#tpsa_lb').text(),
-        tpsa_ub: $('#tpsa_ub').text()
+        tpsa_ub: $('#tpsa_ub').text(),
+        charge_lb: $('#charge_lb').text(),
+        charge_ub: $('#charge_ub').text(),
+        nrb_lb: $('#nrb_lb').text(),
+        nrb_ub: $('#nrb_ub').text()
       }, function (res) {
         $('#ligands').text(res);
       });
@@ -119,20 +119,20 @@ $(function() {
       mwt_ub: $('#mwt_ub').text(),
       logp_lb: $('#logp_lb').text(),
       logp_ub: $('#logp_ub').text(),
-      nrb_lb: $('#nrb_lb').text(),
-      nrb_ub: $('#nrb_ub').text(),
-      hbd_lb: $('#hbd_lb').text(),
-      hbd_ub: $('#hbd_ub').text(),
-      hba_lb: $('#hba_lb').text(),
-      hba_ub: $('#hba_ub').text(),
-      charge_lb: $('#charge_lb').text(),
-      charge_ub: $('#charge_ub').text(),
       ad_lb: $('#ad_lb').text(),
       ad_ub: $('#ad_ub').text(),
       pd_lb: $('#pd_lb').text(),
       pd_ub: $('#pd_ub').text(),
+      hbd_lb: $('#hbd_lb').text(),
+      hbd_ub: $('#hbd_ub').text(),
+      hba_lb: $('#hba_lb').text(),
+      hba_ub: $('#hba_ub').text(),
       tpsa_lb: $('#tpsa_lb').text(),
-      tpsa_ub: $('#tpsa_ub').text()
+      tpsa_ub: $('#tpsa_ub').text(),
+      charge_lb: $('#charge_lb').text(),
+      charge_ub: $('#charge_ub').text(),
+      nrb_lb: $('#nrb_lb').text(),
+      nrb_ub: $('#nrb_ub').text()
     }, function(res) {
       // If server side validation fails, show the tooltips
       if (res != undefined) {
