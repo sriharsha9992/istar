be6f1f951e9b3362b92bdb3e59217258e440da9f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-07-23 21:56:15 +0800

    Displayed genome name instead of taxonomy id

--- public/igrep/index.js
+++ public/igrep/index.js
@@ -3,12 +3,23 @@ $(function () {
   // Fetch email from cookie
   var email = $.cookie('email');
   $('#email').val(email);
+  
+  // Get a genome via its taxonomy id.
+  var genomes = $('option');
+  function getGenome(taxon_id) {
+    var genome;
+    genomes.each(function() {
+      var g = $(this);
+      if (g.val() == taxon_id) genome = g.text();
+    });
+    return genome;
+  }
 
   // Fetch jobs
   $.get('jobs', { email: email }, function(jobs) {
     var jobs;
     jobs.forEach(function(job) {
-      jobs += '<tr><td>' + job.genome + '</td><td>' + $.format.date(new Date(job.submitted), 'yyyy/MM/dd HH:mm:ss') + '</td><td>' + (job.done == undefined ? 'Queued for execution' : $.format.date(new Date(job.done), 'yyyy/MM/dd HH:mm:ss')) + '</td><td><a href="jobs/' + job._id + '/log.csv"><img src="/excel.png" class="csv" alt="log.csv"/></a></td><td><a href="jobs/' + job._id + '/pos.csv"><img src="/excel.png" class="csv" alt="pos.csv"/></a></td></tr>';
+      jobs += '<tr><td>' + getGenome(job.genome) + '</td><td>' + $.format.date(new Date(job.submitted), 'yyyy/MM/dd HH:mm:ss') + '</td><td>' + (job.done == undefined ? 'Queued for execution' : $.format.date(new Date(job.done), 'yyyy/MM/dd HH:mm:ss')) + '</td><td><a href="jobs/' + job._id + '/log.csv"><img src="/excel.png" class="csv" alt="log.csv"/></a></td><td><a href="jobs/' + job._id + '/pos.csv"><img src="/excel.png" class="csv" alt="pos.csv"/></a></td></tr>';
     });
     $('#jobs').html(jobs);
   });
