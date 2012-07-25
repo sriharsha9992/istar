c55f9cb079c91002d9864f5735298d42ccdf8b15
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-07-25 19:58:43 +0800

    Refreshed jobs after submission

--- public/igrep/index.js
+++ public/igrep/index.js
@@ -15,30 +15,36 @@ $(function () {
     return genome;
   }
 
-  // Fetch jobs
+  // Refresh the table of jobs
+  var page = 0;
   function refreshJobs() {
-    $.get('jobs', { email: email }, function(jobs) {
-      // Construct rows in html from jobs
+    $.get('jobs', { email: email, page: page }, function(jobs) {
       var rows;
       jobs.forEach(function(job) {
         var done = job.done != undefined;
         rows += '<tr><td>' + getGenome(job.genome) + '</td><td>' + $.format.date(new Date(job.submitted), 'yyyy/MM/dd HH:mm:ss') + '</td><td>' + (done ? $.format.date(new Date(job.done), 'yyyy/MM/dd HH:mm:ss') : 'Queued for execution') + '</td><td>' + (done ? '<a href="jobs/' + job._id + '/log.csv"><img src="/excel.png" class="csv" alt="log.csv"/></a>' : '') + '</td><td>' + (done ? '<a href="jobs/' + job._id + '/pos.csv"><img src="/excel.png" class="csv" alt="pos.csv"/></a>' : '') + '</td></tr>';
       });
-
-      // Refresh table rows
-      $('#jobs').fadeOut(function () {
-        $(this).html(rows).fadeIn();
-      });
-
-      // Initialize pager
-      var num_jobs = jobs.length;
-      $('#prevpage').click(function () {
-      });
-      $('#nextpage').click(function () {
-      });
+      $('#jobs').hide().html(rows).fadeIn();
     });
   }
+
+  // Initialize the table of jobs
   refreshJobs();
+
+  // Initialize pager
+  var prevpage = $('#prevpage');
+  var nextpage = $('#nextpage');
+  prevpage.addClass('disabled').click(function () {
+    if (prevpage.hasClass('disabled')) return;
+    if (page) page -= 1;
+    refreshJobs();
+  });
+  nextpage.click(function () {
+    if (nextpage.hasClass('disabled')) return;
+    if (!page) prevpage.removeClass('disabled');
+    page += 1;
+    refreshJobs();
+  });
 
   // Initialize tooltips
   $('.control-label a[rel=tooltip]').tooltip();
@@ -60,6 +66,7 @@ $(function () {
         });
         return;
       }
+      page = 0;
       refreshJobs();
       // Save email into cookie
       $.cookie('email', $('#email').val(), { expires: 7 });
