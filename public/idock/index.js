12bf0ae04b062ea5be6100cfa03aafc7cad70575
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-08-01 23:18:10 +0800

    Revised the jobs table and pager for idock

--- public/idock/index.js
+++ public/idock/index.js
@@ -14,15 +14,129 @@ $(function() {
   }
   $('#email').val(email);
 
-  // Fetch jobs
-  $.get('jobs', { email: email }, function(jobs) {
-    jobs.forEach(function(job) {
-    });
-    $('#jobs');
-  });
-
   // Initialize tooltips
   $('.control-label a').tooltip();
+
+  // Initialize pager
+  var last_page, page,
+      frstpage = $('#frstpage'),
+      lastpage = $('#lastpage'),
+      prevpage = $('#prevpage'),
+      nextpage = $('#nextpage'),
+      whatpage = $('#whatpage');
+  frstpage.click(function() {
+    if (frstpage.hasClass('disabled')) return;
+    page = 1;
+    refresh();
+  });
+  lastpage.click(function() {
+    if (lastpage.hasClass('disabled')) return;
+    page = last_page;
+    refresh();
+  });
+  prevpage.click(function() {
+    if (prevpage.hasClass('disabled')) return;
+    page -= 1;
+    refresh();
+  });
+  nextpage.click(function() {
+    if (nextpage.hasClass('disabled')) return;
+    page += 1;
+    refresh();
+  });
+  whatpage.change(function() {
+    var p = parseInt(whatpage.val());
+    if ((p < 1) || (p > last_page) || (p === page)) return;
+    page = p;
+    refresh();
+  });
+
+  // Construct td's of a tr from a job
+  function tr(job) {
+    var tds = new Array(5);
+    if (job === undefined) {
+      tds[0] = '&nbsp;';
+      tds[1] = '&nbsp;';
+      tds[2] = '&nbsp;';
+      tds[3] = '&nbsp;';
+      tds[4] = '&nbsp;';
+      return tds;
+    }
+    var done = job.done != undefined;
+    tds[0] = job.ligands.toString();
+    tds[1] = $.format.date(new Date(job.submitted), 'yyyy/MM/dd HH:mm:ss');
+	tds[2] = 'progress';
+    tds[3] = (done ? $.format.date(new Date(job.done), 'yyyy/MM/dd HH:mm:ss') : 'Queued for execution');
+    tds[4] = (done ? '<a href="jobs/' + job._id + '/result.tar.gz"><img src="/excel.png" alt="result.tar.gz"/></a>' : null);
+    return tds;
+  }
+
+  // Refresh the table of jobs and its pager
+  var jobs, jobs_trs = $('#jobs tr');
+  function refresh() {
+    var offset = 8 * (page - 1);
+    jobs_trs.each(function(i) {
+      var tds = tr(jobs[offset + i]);
+      $('td', $(this)).each(function(j) {
+        $(this).html(tds[j]);
+      });
+    });
+    if (page === 1) {
+      frstpage.addClass('disabled');
+      prevpage.addClass('disabled');
+    } else {
+      frstpage.removeClass('disabled');
+      prevpage.removeClass('disabled');
+    }
+    if (page === last_page) {
+      lastpage.addClass('disabled');
+      nextpage.addClass('disabled');
+    } else {
+      lastpage.removeClass('disabled');
+      nextpage.removeClass('disabled');
+    }
+    whatpage.val(page);
+  }
+
+  // Fade selected cells
+  function fade(row, col) {
+    var offset = 8 * (page - 1);
+    jobs_trs.each(function(i) {
+      if (!row(offset + i)) return;
+      var tds = tr(jobs[offset + i]);
+      $('td', $(this)).each(function(j) {
+        if (col(j)) $(this).hide().html(tds[j]).fadeIn('slow');
+      });
+    });
+  }
+
+  // Initialize the table of jobs
+  var dones;
+  $.get('jobs', { email: email }, function(res) {
+    jobs = res;
+    for (dones = jobs.length; dones && !jobs[dones - 1].done; --dones);
+    last_page = jobs.length ? ((jobs.length + 7) >> 3) : 1;
+    page = last_page;
+    refresh();
+  });
+
+  // Activate the timer to refresh result every second
+/*
+  setInterval(function() {
+    $.get('done', { email: email, skip: dones }, function(res) {
+      if (!res.length) return;
+      res.forEach(function(job, i) {
+        jobs[dones + i].done = job.done;
+      });
+      fade(function(i) {
+        return (dones <= i) && (i < dones + res.length);
+      }, function(j) {
+        return j >= 2;
+      });
+      dones += res.length;
+    });
+  }, 1000);
+*/
 
   // Initialize sliders
   $('#mwt').slider({
@@ -155,6 +269,22 @@ $(function() {
       var expireDate = new Date();
       expireDate.setTime(expireDate.getTime() + (7 * 24 * 60 * 60 * 1000));
       document.cookie = 'email=' + $('#email').val() + ';expires=' + expireDate.toUTCString();
+      // Redirect to the current page if the email is changed
+      if (email !== $('#email').val()) {
+        return window.location.replace('/idock');
+      }
+      // Concat the new job to the existing jobs array, and refresh the table of jobs
+      jobs = jobs.concat(res);
+      last_page = jobs.length ? ((jobs.length + 7) >> 3) : 1;
+      if (page < last_page) {
+        page = last_page;
+        refresh();
+      }
+      fade(function(i) {
+        return i + 1 === jobs.length;
+      }, function(j) {
+        return j <= 3;
+      });
     }, 'json');
   });
 });
