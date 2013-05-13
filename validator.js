38a509b614c59c8500c1349deb14a96ef3afe90f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-05-13 16:17:41 +0800

    Renamed param to key

--- validator.js
+++ validator.js
@@ -4,14 +4,14 @@ validator.Validator.prototype.init = function(obj) {
   this.err = {};
   return this;
 }
-validator.Validator.prototype.chk = function(param, fail_msg, compulsory) {
+validator.Validator.prototype.chk = function(key, fail_msg, compulsory) {
   this.error = function(msg) {
-    if ((this.obj.hasOwnProperty(param) || compulsory) && (!this.err.hasOwnProperty(param))) {
-      this.err[param] = msg;
+    if ((this.obj.hasOwnProperty(key) || compulsory) && (!this.err.hasOwnProperty(key))) {
+      this.err[key] = msg;
     }
     return this;
   }
-  return this.check(this.obj[param], fail_msg);
+  return this.check(this.obj[key], fail_msg);
 }
 validator.Validator.prototype.failed = function() {
   return Object.keys(this.err).length;
@@ -27,11 +27,11 @@ validator.Filter.prototype.init = function(obj) {
   this.res = {};
   return this;
 }
-validator.Filter.prototype.snt = function(param, def) {
+validator.Filter.prototype.snt = function(key, def) {
   this.modify = function(value) {
-    this.res[param] = value;
+    this.res[key] = value;
   }
-  return this.sanitize(this.obj.hasOwnProperty(param) ? this.obj[param]: def);
+  return this.sanitize(this.obj.hasOwnProperty(key) ? this.obj[key]: def);
 }
 validator.Filter.prototype.wrap = function(value) {
   return this;
