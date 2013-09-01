5ae889c080248357724ad570bc6988c884807aed
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-01 20:07:39 +0800

    Removed RF-Score.r and RF-Score-Model12_originalFeats.Rdata

--- idock/RF-Score.r
+++
@@ -1,4 +0,0 @@
-args = commandArgs(trailingOnly = TRUE)
-suppressMessages(library(randomForest))
-load("RF-Score_model12_originalFeats.Rdata")
-write.table(predict(RF_Score, read.csv(args[1])), file = args[2], quote = FALSE, row.names = FALSE, col.names = FALSE)
