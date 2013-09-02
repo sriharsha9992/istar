850dd78aa7e85eef8fd96737802c7c5ed520f944
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-02 09:12:02 +0800

    Made the tables in accordions responsive

--- public/igrep/index.js
+++ public/igrep/index.js
@@ -2632,7 +2632,7 @@ $(function() {
     g.files.forEach(function(f, i) {
       trs[i] = '<tr><td>' + i + '</td><td><a href="genomes/' + g.name + '/' + f.file + '">' + f.file + '</a></td><td>' + f.header + '</td><td style="text-align: right">' + f.nucleotides.comma() + '</td></tr>';
     });
-    return '<h3>' + g.name + ', taxonomy id ' + g.taxid + ', NCBI build ' + g.ncbiBuild + ', version ' + g.version + ', released on ' + g.releaseDate + ', total ' + g.nucleotides.comma() + ' nucleotides in ' + g.files.length + ' files</h3><div><table class="table"><thead><tr><th>Index</th><th>File</th><th>Header</th><th>Nucleotides</th></tr></thead><tbody>' + trs.join('') + '</tbody></table></div>';
+    return '<h3>' + g.name + ', taxonomy id ' + g.taxid + ', NCBI build ' + g.ncbiBuild + ', version ' + g.version + ', released on ' + g.releaseDate + ', total ' + g.nucleotides.comma() + ' nucleotides in ' + g.files.length + ' files</h3><div class="table-responsive"><table class="table table-hover"><thead><tr><th>Index</th><th>File</th><th>Header</th><th>Nucleotides</th></tr></thead><tbody>' + trs.join('') + '</tbody></table></div>';
   }
 
   // Apply accordion to genomes
