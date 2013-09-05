0079ddefb898e2126f8bebca9cd73f0a7dfcd98c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-05 20:18:57 +0800

    Tabified istar.js

--- public/istar.js
+++ public/istar.js
@@ -1,7 +1,7 @@
 // Add a comma every 3 digits, e.g. turn 11386040 into 11,386,040
 var d3 = /(\d+)(\d{3})/;
 Number.prototype.comma = function() {
-  var s = this.toString();
-  while (d3.test(s)) s = s.replace(d3, '$1' + ',' + '$2');
-  return s;
+    var s = this.toString();
+    while (d3.test(s)) s = s.replace(d3, '$1' + ',' + '$2');
+    return s;
 };
