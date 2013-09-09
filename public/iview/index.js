49472c8c7aa3de2b31afb33d20a620a8491904ae
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-09 09:24:51 +0800

    Added class active to the first label in ligand selector

--- public/iview/index.js
+++ public/iview/index.js
@@ -3,6 +3,7 @@ $(function () {
 	hits.html(['ZINC01234568', 'ZINC01234569', 'ZINC01234566', 'ZINC01234567'].map(function(id) {
 		return '<label class="btn btn-primary"><input type="radio">' + id + '</label>';
 	}).join(''));
+	$(':first', hits).addClass('active');
 	$('> .btn', hits).click(function(e) {
 		alert(e.currentTarget.innerText);
 	});
