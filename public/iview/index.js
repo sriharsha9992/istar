01804dbd9517c67475535b5ab2ea405e5abd9e64
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-09 08:47:17 +0800

    Added ligand selector

--- public/iview/index.js
+++ public/iview/index.js
@@ -1,4 +1,11 @@
 $(function () {
+	var hits = $('#hits');
+	hits.html(['ZINC01234568', 'ZINC01234569', 'ZINC01234566', 'ZINC01234567'].map(function(id) {
+		return '<label class="btn btn-primary"><input type="radio">' + id + '</label>';
+	}).join(''));
+	$('> .btn', hits).click(function(e) {
+		alert(e.currentTarget.innerText);
+	});
 	var iv = new iview('iview');
 	var path = '/idock/jobs/' + location.search.substr(1) + '/';
 	$.get(path + 'box.conf', function (b) {
